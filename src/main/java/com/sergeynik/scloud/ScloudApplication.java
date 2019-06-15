ac6983b974e200525e56794df32251828c8822df
sergei.nikolchikov <sergei.nikolchikov@itbrick.ru>
2019-06-15 18:17:06 +0300

    Initial commit

---
+++ src/main/java/com/sergeynik/scloud/ScloudApplication.java
@@ -0,0 +1,13 @@
+package com.sergeynik.scloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScloudApplication.class, args);
+    }
+
+}
